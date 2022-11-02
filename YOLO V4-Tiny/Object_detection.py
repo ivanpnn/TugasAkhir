1cae1cccf7422e43c0bc23d1b03794c088afb4d6
IvanPrasetyoNugroho <ivanprasetyo559@gmail.com>
2022-11-02 22:21:21 +0700

    Counting car

--- YOLO V4-Tiny/Object_detection.py
+++ YOLO V4-Tiny/Object_detection.py
@@ -12,8 +12,10 @@ MIN_CONFIDENCE=0.5
 
 timeframe= time.time()
 frame_id = 0
+carCount = 0
 
-def pedestrian_detection(image, model, layer_name, personidz=0):
+def pedestrian_detection(image, model, layer_name, personidz=3):
+	global carCount
 	(H, W) = image.shape[:2]
 	results = []
 	#Mengatur kualitas video
@@ -24,16 +26,15 @@ def pedestrian_detection(image, model, layer_name, personidz=0):
 	boxes = []
 	centroids = []
 	confidences = []
+	classIDs = []
 
 	for output in layerOutputs:
 		for detection in output:
-
 			scores = detection[5:]
 			classID = np.argmax(scores)
 			confidence = scores[classID]
 
-			if classID ==  personidz and confidence > MIN_CONFIDENCE:
-
+			if confidence > MIN_CONFIDENCE:
 				box = detection[0:4] * np.array([W, H, W, H])
 				(centerX, centerY, width, height) = box.astype("int")
 
@@ -43,7 +44,13 @@ def pedestrian_detection(image, model, layer_name, personidz=0):
 				boxes.append([x, y, int(width), int(height)])
 				centroids.append((centerX, centerY))
 				confidences.append(float(confidence))
-				
+				classIDs.append(classID)
+				# print("ClassID = "+str(classID))
+
+				if (int(image.shape[0]/2) - 2) < y < (int(image.shape[0]/2)+2):
+					if classID == 2:
+						carCount = carCount + 1
+
 
 						
 	# apply non-maxima suppression to suppress weak, overlapping
@@ -57,10 +64,16 @@ def pedestrian_detection(image, model, layer_name, personidz=0):
 			(x, y) = (boxes[i][0], boxes[i][1])
 			(w, h) = (boxes[i][2], boxes[i][3])
 			confidence = confidences[i]
+
 			# update our results list to consist of the person
 			# prediction probability, bounding box coordinates,
 			# and the centroid
-			cv2.putText(frame, "car" +" "+ str(round(confidence, 2)), (x, y), font, 1, (0, 255, 0), 2)
+			label = str(LABELS[classIDs[i]])
+			cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y), font, 1, (0, 255, 0), 2)
+
+			cv2.line(image, (0, int(image.shape[0]/1.5)+3), (int(image.shape[1]), int(image.shape[0]/1.5)+3), (0, 0, 200), 1)
+			cv2.line(image, (0, int(image.shape[0]/1.5)-3), (int(image.shape[1]), int(image.shape[0]/1.5)-3), (0, 0, 200), 1)
+
 
 			res = (confidences[i], (x, y, x + w, y + h), centroids[i])
 			results.append(res)
@@ -87,7 +100,7 @@ model.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
 layer_name = model.getLayerNames()
 layer_name = [layer_name[i - 1] for i in model.getUnconnectedOutLayers()]
-cap = cv2.VideoCapture("2Person2.mp4")
+cap = cv2.VideoCapture("traffic.mp4")
 writer = None
 font = cv2.FONT_HERSHEY_PLAIN
 
@@ -100,8 +113,7 @@ while True:
 	if not grabbed:
 		break
 	frame = imutils.resize(frame, width=700)
-	results = pedestrian_detection(frame, model, layer_name,
-		personidz=LABELS.index("car"))
+	results = pedestrian_detection(frame, model, layer_name, personidz=LABELS.index("car"))
 
 	for res in results:
 		cv2.rectangle(frame, (res[1][0],res[1][1]), (res[1][2],res[1][3]), (0, 255, 0), 2)
@@ -112,8 +124,10 @@ while True:
 	cv2.putText(frame, "FPS", (120, 50), font, 2, (0, 255, 0), 2)	
 	#counter
 	count1 = len(results) 
-	cv2.putText(frame, "jumlah" +" "+ str(count1), (10, 80), font, 2, (0, 255, 0))
-	
+	cv2.putText(frame, "jumlah" +" "+ str(carCount), (10, 80), font, 2, (0, 255, 0))
+
+	print("carCount =" + str(carCount))
+
 	cv2.imshow("Detection",frame)
 
 	key = cv2.waitKey(1)
